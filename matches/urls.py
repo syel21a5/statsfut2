10e02a5b0739f51dd428faeec9abe68380e6fe85
Syel (server) <syel@statsfut.com>
2026-09-22 06:31:59 -0300

    Corrige rota para a página do CornerPro em /stats/cornerpro/argentina/liga-profesional

--- matches/urls.py
+++ matches/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('stats/<str:country_name>/<str:league_name>/', views.league_stats, name='country_league_stats'),
     path('stats/<str:country_name>/<str:league_name>/cornerpro/', views.cornerpro_league_stats, name='cornerpro_league_stats'),
     path('cornerpro/', views.cornerpro_home, name='cornerpro_home'),
+    path('stats/cornerpro/<str:country_name>/<str:league_name>/', views.cornerpro_league_stats, name='cornerpro_league_stats'),
 ]
